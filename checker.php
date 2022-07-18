483056f4bba05c995819df59920c5ca78388e68e
byIx <40878600+byIx@users.noreply.github.com>
2022-07-19 04:22:45 +0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,1 +1,1 @@
-{ "version":"1.0.0", "longversion":10000, "released":1658182200, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/111\/" }
+{ "version":"1.0.0", "longversion":10000, "released":1658182200, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2686-k4-member-rank-progress\/" }
